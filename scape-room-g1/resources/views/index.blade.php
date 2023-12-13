ba2622f753b648c79d7ee96a8dfa4fa7e8bd6d6c
nexustor <nexustor@gmail.com>
2023-12-13 14:16:11 +0100

    index

---
+++ scape-room-g1/resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="bg-black w-full h-52 md:h-20">
+        <h1 class="text-white text-center font-medium p-5 text-7xl md:text-4xl">SCAPE ROOM</h1>
+    </div>
+    <div class="w-full bg-blue-500 h-auto">
+        <div>hola</div>
+    </div>
+    <div class="bg-black w-full h-25 p-4">
+        <p class="text-white">Colaboradores:</p>
+        <p class="text-white">Alumnos de 2DW3A y dpto. Química</p>
+        <p class="text-white font-medium">Email:alumnos@txurdinaga.eus</p>
+        <p class="text-white font-medium">telf:555-222-123</p>
+    </div>
+</body>
+</html>
