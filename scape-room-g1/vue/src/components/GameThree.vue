ea4913166d08157a1cd41fcb95fa0eff504fc835
nexustor <nexustor@gmail.com>
2023-12-15 12:15:28 +0100

    ahorcado diseño acabado

--- scape-room-g1/vue/src/components/GameThree.vue
+++ scape-room-g1/vue/src/components/GameThree.vue
@@ -7,8 +7,15 @@
                 <main class="m-5">
                     <p class="border-transparent shadow-xl p-2 rounded-lg m-1 text-center m-auto bg-gradient-to-r from-slate-300 to-slate-400">Introduce las letras correctas. Pero CUIDADO!!! o enviaras anuestro amigo Kenny al otro barrio</p>
                 </main>
-
-
+            <div class="pb-5 mt-40 mb-12">
+                <img src="../assets/hangman11.png" alt="" class="mx-auto" >
+            </div>
+            <div class="mx-auto p-5 text-center mb-10 text-4xl">
+                <h1>_E_L_T_Y</h1>
+            </div>
+            <div>
+                <h2 class="bg-black text-gray-50 py-8 text-4xl text-center">Esto es el footer</h2>
+            </div>
     </div>
 
 
