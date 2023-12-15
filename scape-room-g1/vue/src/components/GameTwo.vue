9531132d2bc10c2d0bd3ca5b4e977a73d287a052
XabierTrabajo <xabier.trabajo.69@gmail.com>
2023-12-15 13:51:08 +0100

    game 2 design

--- scape-room-g1/vue/src/components/GameTwo.vue
+++ scape-room-g1/vue/src/components/GameTwo.vue
@@ -4,76 +4,91 @@
             <h1 class="text-4xl text-center font-bold text-gray-50 bg-black px-3 py-8 m-1">Agrupando</h1>
         </header>
 
-            <main class="m-5">
-                <p class="border-transparent shadow-xl p-2 rounded-lg m-1 text-center m-auto bg-gradient-to-r from-slate-300 to-slate-400">Trabaja tu memoria volteado las tarjetas y juntando las imagenes con los nombres</p>
-            </main>
+        <main class="m-5">
+            <p class="border-transparent shadow-xl p-2 rounded-lg m-1 text-center m-auto bg-gradient-to-r from-slate-300 to-slate-400">
+                Demuestra tu conocimiento agrupando los compuestos con sus categorías.
+            </p>
+        </main>
 
-            <div class="grid grid-cols-2 gap-5 items-center m-5 justify-items-center m-5 ">
-                <div v-for="card in cards" class="w-32 h-32 bg-gradient-to-r from-slate-300 to-slate-400 rounded-xl shadow-lg p-3 text-center align-center md:hover:scale-110">
-                    <p class="hidden " >{{ card.name }}</p>
+        <!-- Los divs de las categorias -->
+        <div class="container-fluid">
+            <div class="grid grid-cols-1 sm:grid-cols-4 p-0 text-center">
+                <div class="bg-gradient-to-r from-yellow-300 to-orange-500 p-1 h-32">
+                    Analisis
                 </div>
-                <div v-for="card in cards" class="w-32 h-32 bg-gradient-to-r from-slate-300 to-slate-400 rounded-xl shadow-lg p-3 text-center align-center">
-                    <p class="hidden ">{{ card.img }}</p>
+                <div class="bg-gradient-to-r from-green-400 to-green-800 p-1 h-32">
+                    Microbiología
+                </div>
+                <div class="bg-gradient-to-r from-blue-400 to-indigo-800 p-1 h-32">
+                    Medida
+                </div>
+                <div class="bg-gradient-to-r from-zinc-300 to-zinc-500 p-1 h-32">
+                    Biotecnología
                 </div>
             </div>
         </div>
+
+
+
+        <!-- El div de los compuestos -->
+        <div class="container-fluid my-32">
+            <div class="grid grid-cols-2 sm:grid-cols-4 p-0 text-center">
+                <div class="border-2 border-gray-950 bg-gradient-to-r from-stone-300 via-zinc-400 to-stone-300 rounded m-1" v-for="compoundElement in compoundTestData">
+                    {{ compoundElement.compound }} {{ compoundElement.category }}
+                </div>
+            </div>
+
+        </div>
+</div>
 
 </template>
 <script>
 export default{
     data() {
         return {
-            cards:[],
-            FormulaNames:[{//los objetos estan hardcodeados para hacer pruebas, deaspues se tendrán que recoger de la BD
-                name:'Formaldehído',
-                img:'Formaldehyde.svg'
-            },{
-                name:'Galactosa',
-                img:'Beta-D-Galactopyranose.svg'
-            },{
-                name:'Ribosa',
-                img:'D-ribosa.svg'
-            },{
-                name:'Piruvato',
-                img:'Pyruvate_decarb_1.svg'
-            },{
-                name:'Acido Lactico',
-                img:'Lactic-acid-skeletal.svg'
-            },{
-                name:'Cloruro de Bencilo',
-                img:'Benzyl_chloride.svg'
-            },{
-                name:'Fenol',
-                img:'Phenol_chemical_structure_es.svg'
-            },{
-                name:'Anisol',
-                img:'Anisol.svg'
-            },{
-                name:'Acetato Sódico',
-                img:'Sodium_acetate_formula_V_1.svg'
-            },{
-                name:'Benzoato de etilo',
-                img:'Ethyl_benzoate.svg'
-            },{
-                name:'Acetato de Metilo',
-                img:'Methyl_acetate.svg'
-            },{
-                name:'Anhídrido Benzoico',
-                img:'Benzoic_anhydride.svg'
-            }]
+            // datos de prueba para el diseño
+            compoundTestData: [
+                {
+                    compound: 'compuesto 1',
+                    category: 'ANALISIS'
+                },
+                {
+                    compound: 'compuesto 2',
+                    category: 'ANALISIS'
+                },
+                {
+                    compound: 'compuesto 3',
+                    category: 'MICROBIOLOGÍA'
+                },
+                {
+                    compound: 'compuesto 4',
+                    category: 'MICROBIOLOGÍA'
+                },
+                {
+                    compound: 'compuesto 5',
+                    category: 'MEDIDA'
+                },
+                {
+                    compound: 'compuesto 6',
+                    category: 'MEDIDA'
+                },
+                {
+                    compound: 'compuesto 7',
+                    category: 'BIOTECNOLOGÍA'
+                },
+                {
+                    compound: 'compuesto 8',
+                    category: 'BIOTECNOLOGÍA'
+                }
+            ],
+            compoundData: []
         }
     },
     methods:{
-        getNames(){
-            for(let i=0;i<4;i++){
-               let formulas=Math.floor(Math.random()*(this.FormulaNames.length));
-            this.cards.push(this.FormulaNames[formulas])
-            }
-            return this.cards;
-        }
+
     },
     mounted() {
-        this.getNames();
+
     },
 
 
