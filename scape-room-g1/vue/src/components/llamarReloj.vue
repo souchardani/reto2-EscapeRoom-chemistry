6ecea060b1b472105526f4eae69f0fcccdd9c1e4
souchardani <114416167+souchardani@users.noreply.github.com>
2023-12-18 08:37:12 +0100

    sin reloj para mergear

---
+++ scape-room-g1/vue/src/components/llamarReloj.vue
@@ -0,0 +1,30 @@
+<template>
+    <Reloj ref="relojContador"></Reloj>
+    <button class="bg-blue-500 py-2 px-4 rounded-full m-5" @click="reduceTime">
+        Reducir tiempo
+    </button>
+</template>
+
+<script>
+import Reloj from "../components/Reloj.vue";
+
+export default {
+    name: "GameTwo",
+    data() {
+        return {
+            // Variables
+        };
+    },
+    methods: {
+        reduceTime() {
+            this.$refs.relojContador.reduceTime(60); // Reducir 1 minuto (60 segundos)
+        },
+    },
+    mounted() {
+        // Mounted
+    },
+    components: {
+        Reloj,
+    },
+};
+</script>
--- scape-room-g1/vue/src/views/GameTwo.vue
+++ scape-room-g1/vue/src/views/GameTwo.vue
@@ -1,30 +1,3 @@
-<template>
-    <Reloj ref="relojContador"></Reloj>
-    <button class="bg-blue-500 py-2 px-4 rounded-full m-5" @click="reduceTime">
-        Reducir tiempo
-    </button>
-</template>
+<template></template>
 
-<script>
-import Reloj from "../components/Reloj.vue";
-
-export default {
-    name: "GameTwo",
-    data() {
-        return {
-            // Variables
-        };
-    },
-    methods: {
-        reduceTime() {
-            this.$refs.relojContador.reduceTime(60); // Reducir 1 minuto (60 segundos)
-        },
-    },
-    mounted() {
-        // Mounted
-    },
-    components: {
-        Reloj,
-    },
-};
-</script>
+<script></script>
