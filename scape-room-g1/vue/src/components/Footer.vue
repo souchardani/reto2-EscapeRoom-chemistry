f9c7ad4b8505fb5327b41c6a1f37d02aae79d602
souchardani <114416167+souchardani@users.noreply.github.com>
2023-12-20 11:56:34 +0100

    creado componentes y pagina juego 1

---
+++ scape-room-g1/vue/src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- footer transparente -->
+    <div class="w-100 py-3 my-6">
+        <div
+            class="text-justify relative px-8 py-10 bg-white shadow-lg rounded-3xl sm:rounded-3xl bg-clip-padding bg-opacity-60 border border-gray-200"
+            style="backdrop-filter: blur(20px)"
+        >
+            <div class="mx-auto">
+                <div>
+                    <div class="text-black w-full h-25 p-4">
+                        <div class="my-1">
+                            <span class="font-medium">Colaboradores:</span>
+                            <span class=""
+                                >Alumnos de 2DW3A y dpto. Química</span
+                            >
+                        </div>
+                        <div class="my-1">
+                            <span class="font-medium">Email: </span>
+                            <span>alumnos@txurdinaga.eus</span>
+                        </div>
+                        <div class="my-1">
+                            <span class="font-medium">Telf: </span>
+                            <span>555-222-123</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
