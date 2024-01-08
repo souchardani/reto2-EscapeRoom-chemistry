9a24a9eba9578b490aecee3cd3a145887610d686
nexustor <nexustor@gmail.com>
2024-01-08 14:18:39 +0100

    ahorcado

---
+++ scape-room-g1/vue/src/components/Ahorcadowords.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <label for="">Introduce una letra</label><br><br>
+        <input type="text" id="letra" maxlength="1" size="1" ><br><br>
+        <GlassBtn @click="mostrarLetra()">pulsar</GlassBtn>
+    </div>
+    <div>
+        <h1>{{ palabraBarra }}</h1>
+        <h2>{{ mostrar }}</h2>
+    </div>
+</template>
+
+<script>
+import GlassBtn from "../components/GlassBtn.vue";
+export default {
+    components:{
+        GlassBtn,
+    },
+    data() {
+        return {
+            palabras:["botella","manzana","escoba"],
+            random:"",
+            oculto:"",
+            palabraBarra:"",
+            mostrar:[],
+            letra:"",
+        }
+    },
+    methods: {
+        insertarPalabra(){
+            this.random=this.palabras[Math.floor(Math.random()*this.palabras.length)];
+            return this.random;
+        },
+        ocultarPalabra(){
+            this.oculto=this.random.length;
+
+            for (let i = 0; i < this.oculto; i++) {
+                this.palabraBarra += "_ ";
+
+            }
+        },
+        mostrarLetra(){
+            this.letra=document.getElementById('letra').value;
+
+            for (let i = 0; i < this.random.length; i++) {
+               if (this.letra==this.random[i]) {
+                    alert('has acertado');
+                    // this.mostrar+=this.random[i];
+                    this.mostrar[i]=this.letra;
+               }else{
+                    this.mostrar[i]="_";
+               }
+
+
+
+            }
+        },
+
+    },
+    mounted(){
+        this.insertarPalabra();
+        this.ocultarPalabra();
+        this.mostrarLetra();
+    }
+}
+</script>
--- scape-room-g1/vue/src/views/GameThree.vue
+++ scape-room-g1/vue/src/views/GameThree.vue
@@ -3,6 +3,20 @@
         <img src="../assets/hangman11.png" alt="" class="mx-auto" />
     </div>
     <div class="mx-auto p-5 text-center mb-10 text-4xl">
-        <h1>_E_L_T_Y</h1>
+        <Ahorcadowords></Ahorcadowords>
     </div>
+
 </template>
+<script>
+import Ahorcadowords from '../components/Ahorcadowords.vue';
+
+
+export default{
+    components: { Ahorcadowords }
+};
+
+
+
+
+
+</script>
