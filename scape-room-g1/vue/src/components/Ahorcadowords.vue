78985bb87259fa47857e37816ae65e8e23e1d0d5
nexustor <nexustor@gmail.com>
2024-01-09 13:20:37 +0100

    ahorcado 2

--- scape-room-g1/vue/src/components/Ahorcadowords.vue
+++ scape-room-g1/vue/src/components/Ahorcadowords.vue
@@ -1,21 +1,31 @@
 <template>
     <div>
-        <label for="">Introduce una letra</label><br><br>
+        <label for="">Introduce una letra</label>
+        <div class="pb-5 mt-20 mb-12">
+
+            <img :src="imagen" class='mx-auto'>
+
+        </div>
         <input type="text" id="letra" maxlength="1" size="1" ><br><br>
-        <GlassBtn @click="mostrarLetra()">pulsar</GlassBtn>
+        <button class="rounded-2xl bg-white bg-opacity-40 px-5 py-1.5 mb-5 text-2xl font-semibold text-gray-900 shadow-sm hover:bg-red-500 hover:text-white cursor-pointer focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+            style="backdrop-filter: blur(20px)" @click="mostrarLetra()">pulsar</button><br>
+
     </div>
     <div>
-        <h1>{{ palabraBarra }}</h1>
+        <!-- <h1>{{ palabraBarra }}</h1> -->
         <h2>{{ mostrar }}</h2>
+        <!-- <h1>{{ imagen }}</h1> -->
     </div>
 </template>
 
 <script>
-import GlassBtn from "../components/GlassBtn.vue";
+
+
+
+
 export default {
-    components:{
-        GlassBtn,
-    },
+
+
     data() {
         return {
             palabras:["botella","manzana","escoba"],
@@ -24,6 +34,8 @@ export default {
             palabraBarra:"",
             mostrar:[],
             letra:"",
+            imagen: "../assets/game3_hangman_img/hangman1.png",
+
         }
     },
     methods: {
@@ -31,35 +43,30 @@ export default {
             this.random=this.palabras[Math.floor(Math.random()*this.palabras.length)];
             return this.random;
         },
-        ocultarPalabra(){
-            this.oculto=this.random.length;
-
-            for (let i = 0; i < this.oculto; i++) {
-                this.palabraBarra += "_ ";
-
-            }
-        },
         mostrarLetra(){
             this.letra=document.getElementById('letra').value;
 
             for (let i = 0; i < this.random.length; i++) {
+
                if (this.letra==this.random[i]) {
                     alert('has acertado');
                     // this.mostrar+=this.random[i];
                     this.mostrar[i]=this.letra;
-               }else{
-                    this.mostrar[i]="_";
                }
 
+               if(this.letra!=this.random[i] ){
+                    // this.imagen='../assets/game3_hangman_img/hangman1.png'
+               }
 
-
+               if (this.mostrar[i]==null) {
+                this.mostrar[i]="_";
+               }
             }
         },
 
     },
     mounted(){
         this.insertarPalabra();
-        this.ocultarPalabra();
         this.mostrarLetra();
     }
 }
--- scape-room-g1/vue/src/views/GameThree.vue
+++ scape-room-g1/vue/src/views/GameThree.vue
@@ -1,8 +1,7 @@
 <template>
-    <div class="pb-5 mt-40 mb-12">
-        <img src="../assets/hangman11.png" alt="" class="mx-auto" />
-    </div>
+
     <div class="mx-auto p-5 text-center mb-10 text-4xl">
+
         <Ahorcadowords></Ahorcadowords>
     </div>
 
