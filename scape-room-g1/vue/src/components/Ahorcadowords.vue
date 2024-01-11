8e930dda50d25062eb15f649942ef604e55bbd1b
nexustor <nexustor@gmail.com>
2024-01-11 14:12:54 +0100

    ahorcado modals

--- scape-room-g1/vue/src/components/Ahorcadowords.vue
+++ scape-room-g1/vue/src/components/Ahorcadowords.vue
@@ -16,15 +16,20 @@
         <h2>{{ mostrar }}</h2>
         <!-- <h1>{{ imagen }}</h1> -->
     </div>
+    <success v-bind:enhorabuena="enhorabuena" @clicked2="closeModal"></success>
+    <unsuccess v-bind:mostrar="mostrarm" @clicked="closeModal"></unsuccess>
 </template>
 
 <script>
-
-
-let z=1;
+let z=0;
+let aciertos=0;
+import ProgressBar from "../components/ProgressBar.vue";
+import { mapWritableState,mapActions  } from "pinia";
+import {useProgressBarStore} from "../store/progressBar";
+import unsuccess from "../components/modals/unsuccess.vue";
+import success from "../components/modals/success.vue";
 
 export default {
-
 
     data() {
         return {
@@ -35,6 +40,8 @@ export default {
             mostrar:[],
             letra:"",
             imagen: "",
+            mostrarm: false,
+            enhorabuena:false,
 
         }
     },
@@ -49,35 +56,85 @@ export default {
 
                 // Oculta la palabra con _ _ _
                 for (let i = 0; i < this.random.length; i++) {
+
                     if (this.mostrar[i]==null) {
                         this.mostrar[i]="_";
                     }
 
                     // Si es correcta la letra
                     if (this.letra==this.random[i]) {
-                            alert('has acertado');
-                            // this.mostrar+=this.random[i];
+
                             this.mostrar[i]=this.letra;
                             acertado=true;
+                            aciertos++;
+
 
                     }
+
 
 
                 }
 
                 // si al recorrer la palabra falla
 
-                if(acertado==false && z<12) {
+                if(acertado==false && z<6) {
                     this.imagen="../../public/game3_hangman_img/hangman"+z+".png";
+                    this.marcaError(z);
                     z=z+1;
                 }
+                if(z==6){
+                    this.mostrarm=true;
+                }
+
+                // if(this.mostrar.filter())
 
         },
+        closeModal() {
+            this.mostrarm = false;
+            this.enhorabuena = false;
+            this.$router.push("StartGame");
+        },
+        ...mapActions(useProgressBarStore, [
+            "insertaFallo1",
+            "insertaFallo2",
+            "insertaFallo3",
+            "insertaFallo4",
+            "insertaFallo5",
+            "incrementafallo",
 
+        ]),
+
+        marcaError(contador) {
+            switch (contador) {
+                case 1:
+                    this.insertaFallo1();
+                    break;
+                case 2:
+                    this.insertaFallo2();
+                    break;
+                case 3:
+                    this.insertaFallo3();
+                    break;
+                case 4:
+                    this.insertaFallo4();
+                    break;
+                case 5:
+                    this.insertaFallo5();
+                    break;
+            }
+    },
     },
     mounted(){
         this.insertarPalabra();
         this.mostrarLetra();
+    },
+    components:{
+        ProgressBar,
+        unsuccess,
+        success
+    },
+    computed:{
+        ...mapWritableState(useProgressBarStore,["contador"]),
     }
 }
 </script>
