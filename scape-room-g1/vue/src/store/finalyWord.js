6b47a92b843fd91d9735a2da40c554a9e940e93f
David Arrebola <d.arrebola.porras@gmail.com>
2024-01-16 13:20:07 +0100

    comienzo el store de finalyWord

---
+++ scape-room-g1/vue/src/store/finalyWord.js
@@ -0,0 +1,16 @@
+import { defineStore } from "pinia";
+export const useFinalyWord= defineStore('finalyWord',{
+    //variables en state
+    state:()=>{
+        return{
+            
+        }
+
+    },
+    getters:{
+
+    },
+    actions:{
+
+    }
+})
