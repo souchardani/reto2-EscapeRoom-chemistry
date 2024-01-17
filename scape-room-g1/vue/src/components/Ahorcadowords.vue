281b5dd7640c836da09e7f4181eebbb873869043
nexustor <nexustor@gmail.com>
2024-01-17 13:22:24 +0100

    sonidos

--- scape-room-g1/vue/src/components/Ahorcadowords.vue
+++ scape-room-g1/vue/src/components/Ahorcadowords.vue
@@ -1,5 +1,13 @@
 <template>
     <div>
+        <div class="mt-4 p-4 flex w-20">
+            <GlassBtn  class="mx-3" @click="apagar()">
+                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="18" viewBox="0 0 576 512"><path d="M301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3zM425 167l55 55 55-55c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-55 55 55 55c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-55-55-55 55c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l55-55-55-55c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0z"/></svg>
+            </GlassBtn>
+            <GlassBtn  @click="encender()">
+                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3zM412.6 181.5C434.1 199.1 448 225.9 448 256s-13.9 56.9-35.4 74.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C393.1 284.4 400 271 400 256s-6.9-28.4-17.7-37.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5z"/></svg>
+            </GlassBtn>
+        </div>
         <label for="">Introduce una letra</label>
         <div class="pb-5 mt-20 mb-12">
 
@@ -48,9 +56,25 @@ export default {
             imagen: "",
             mostrarm: false,
             enhorabuena:false,
+            audioAcertado:new Audio('../../public/sonidos/1200.mp3'),
+            audioIncorrecto:new Audio('../../public/sonidos/incorrect-cbt-sound.mp3'),
+            aplausos:new Audio('../../public/sonidos/claps-44774.mp3'),
+            fail:new Audio('../../public/sonidos/fail-144746.mp3'),
         }
     },
     methods: {
+        apagar(){
+            this.audioAcertado.muted=true;
+            this.audioIncorrecto.muted=true;
+            this.aplausos.muted=true;
+            this.fail.muted=true;
+        },
+        encender(){
+            this.audioAcertado.muted=false;
+            this.audioIncorrecto.muted=false;
+            this.aplausos.muted=false;
+            this.fail.muted=false;
+        },
         vaciar(){
             document.getElementById('letra').value="";
         },
@@ -77,6 +101,7 @@ export default {
                     if (this.letra==letram[i]) {
                             acertado=true;
                             this.mostrar[i]=this.letra;
+                            this.audioAcertado.play();
                             document.getElementById('letra').style.borderColor="green";
                     }
 
@@ -88,6 +113,7 @@ export default {
                     this.imagen="../../public/game3_hangman_img/hangman"+z+".png";
                     this.marcaError(z);
                     z=z+1;
+                    this.audioIncorrecto.play();
                     document.getElementById('letra').style.borderColor="red";
                 }
                 // para que el inicio del juego el input sea de color blanco
@@ -100,6 +126,7 @@ export default {
                 // cuando haces 6 fallos se termina el
                 }
                 if(z==6){
+                    this.fail.play();
                     this.mostrarm=true;
                     this.reduceTime(300);
 
@@ -108,6 +135,7 @@ export default {
 
                 //si se muestra toda la palabra
                 if(this.mostrar.includes("_")==false){
+                    this.aplausos.play();
                     this.enhorabuena=true;
                     this.changeJuego3();
                 }
