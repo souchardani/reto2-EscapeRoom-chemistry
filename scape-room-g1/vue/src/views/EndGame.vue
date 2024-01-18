00b6f710d970ce0462a2519e740ea61e7b033442
David Arrebola <d.arrebola.porras@gmail.com>
2024-01-18 13:11:43 +0100

    juego5 con las pistas

--- scape-room-g1/vue/src/views/EndGame.vue
+++ scape-room-g1/vue/src/views/EndGame.vue
@@ -1,42 +1,99 @@
 <template>
 
     <div class="container-fluid flex mx-4 p-4 justify-center items-center">
-        <h1 :cientifico="cientifico" class="text-5xl font-medium">{{ this.cientifico }}</h1>
+        <h1 class="text-4xl font-medium md:text-5xl">{{ this.cientifico }}</h1>
     </div>
     <div class="container-fluid flex justify-center items-center mx-4 p-4">
-        <input type="text" id="1" maxlength="1" v-model="input1" class="flex w-[100px] h-[100px] md:w-[150px] md:h-[150px] xl:w-[200px] xl:h-[200px] bg-white shadow-lg rounded-3xl bg-opacity-60 border border-gray-200 mx-1 md:mx-2 text-center justify-center items-center my-auto text-5xl font-medium ">
-        <input type="text" id="2" maxlength="1" v-model="input2" class="flex w-[100px] h-[100px] md:w-[150px] md:h-[150px] xl:w-[200px] xl:h-[200px] bg-white shadow-lg rounded-3xl bg-opacity-60 border border-gray-200 mx-1 md:mx-2 text-center justify-center items-center my-auto text-5xl font-medium ">
-        <input type="text" id="3" maxlength="1" v-model="input3" class="flex w-[100px] h-[100px] md:w-[150px] md:h-[150px] xl:w-[200px] xl:h-[200px] bg-white shadow-lg rounded-3xl bg-opacity-60 border border-gray-200 mx-1 md:mx-2 text-center justify-center items-center my-auto text-5xl font-medium ">
-        <input type="text" id="4" maxlength="1" v-model="input4" class="flex w-[100px] h-[100px] md:w-[150px] md:h-[150px] xl:w-[200px] xl:h-[200px] bg-white shadow-lg rounded-3xl bg-opacity-60 border border-gray-200 mx-1 md:mx-2 text-center justify-center items-center my-auto text-5xl font-medium ">
-        <input type="text" id="5" maxlength="1" v-model="input5" class="flex w-[100px] h-[100px] md:w-[150px] md:h-[150px] xl:w-[200px] xl:h-[200px] bg-white shadow-lg rounded-3xl bg-opacity-60 border border-gray-200 mx-1 md:mx-2 text-center justify-center items-center my-auto text-5xl font-medium ">
+        <input type="text" id="1" maxlength="1" v-model="words[0].input" :class="['flex','w-[70px]','h-[70px]','md:w-[150px]','md:h-[150px]','xl:w-[200px]','xl:h-[200px]',{'bg-red-500':words[0].estado=='rojo','bg-green-500':words[0].estado=='verde',},{'bg-white': words[0].estado == undefined},'shadow-lg','rounded-3xl','bg-opacity-60','border','border-gray-200','mx-1','md:mx-2,','text-center','justify-center','items-center','my-auto','text-3xl','md:text-5xl','font-medium']" @keyup="comprobarClave(0)">
+        <input type="text" id="2" maxlength="1" v-model="words[1].input" :class="['flex','w-[70px]','h-[70px]','md:w-[150px]','md:h-[150px]','xl:w-[200px]','xl:h-[200px]',{'bg-red-500':words[1].estado=='rojo','bg-green-500':words[1].estado=='verde',},{'bg-white': words[0].estado == undefined},'shadow-lg','rounded-3xl','bg-opacity-60','border','border-gray-200','mx-1','md:mx-2,','text-center','justify-center','items-center','my-auto','text-3xl','md:text-5xl','font-medium']" @keyup="comprobarClave(1)">
+        <input type="text" id="3" maxlength="1" v-model="words[2].input" :class="['flex','w-[70px]','h-[70px]','md:w-[150px]','md:h-[150px]','xl:w-[200px]','xl:h-[200px]',{'bg-red-500':words[2].estado=='rojo','bg-green-500':words[2].estado=='verde',},{'bg-white': words[0].estado == undefined},'shadow-lg','rounded-3xl','bg-opacity-60','border','border-gray-200','mx-1','md:mx-2,','text-center','justify-center','items-center','my-auto','text-3xl','md:text-5xl','font-medium']" @keyup="comprobarClave(2)">
+        <input type="text" id="4" maxlength="1" v-model="words[3].input" :class="['flex','w-[70px]','h-[70px]','md:w-[150px]','md:h-[150px]','xl:w-[200px]','xl:h-[200px]',{'bg-red-500':words[3].estado=='rojo','bg-green-500':words[3].estado=='verde',},{'bg-white': words[0].estado == undefined},'shadow-lg','rounded-3xl','bg-opacity-60','border','border-gray-200','mx-1','md:mx-2,','text-center','justify-center','items-center','my-auto','text-3xl','md:text-5xl','font-medium']" @keyup="comprobarClave(3)">
+        <input type="text" id="5" maxlength="1" v-model="words[4].input" :class="['flex','w-[70px]','h-[70px]','md:w-[150px]','md:h-[150px]','xl:w-[200px]','xl:h-[200px]',{'bg-red-500':words[4].estado=='rojo','bg-green-500':words[4].estado=='verde',},{'bg-white': words[0].estado == undefined},'shadow-lg','rounded-3xl','bg-opacity-60','border','border-gray-200','mx-1','md:mx-2,','text-center','justify-center','items-center','my-auto','text-3xl','md:text-5xl','font-medium']" @keyup="comprobarClave(4)">
     </div>
+    <unsuccess v-bind:mostrar="mostrar" @clicked="closeModal"></unsuccess>
 
 </template>
 <script>
 import { useFinalyWord } from '../store/finalyWord';
-import { mapWritableState } from 'pinia';
+import { useProgressBarStore } from '../store/progressBar';
+import { mapWritableState,mapActions } from 'pinia';
+import { useTemporizadorStore } from '../store/TemporizadorStore';
+import unsuccess from '../components/modals/unsuccess.vue';
 export default{
     data() {
         return {
-            cientifico:"Henri Moissan",//esta variable es el nombre el cientifico que daremos como pista
-            pista:"F1886",//esta variable es la pista completa que tienen que resolver
+            //esta variable es el nombre el cientifico que daremos como pista
+            pista:this.clave,//esta variable es la pista completa que tienen que resolver
             words:[
-                {input1:""},
-                {input2:""},
-                {input3:""},
-                {input4:""},
-                {input5:""},
+                {input:"",estado:""},
+                {input:"",estado:""},
+                {input:"",estado:""},
+                {input:"",estado:""},
+                {input:"",estado:""},
             ],
+            estado:true,
+            contador:0,
+            mostrar:false,
+            aciertos:0
         }
 
     },
     methods:{
-        siguienteinput(){
+        comprobarClave(indice){
+                    if(this.words[indice].input==""){
+                        this.words[indice].estado="";
+                    }else {
+                        if(this.words[indice].input.toLocaleUpperCase()===this.pista[indice]){
+                            this.words[indice].estado="verde";
+                            this.aciertos++;
+                            this.compruebaAciertos(this.aciertos);
+                        }else if (this.words[indice].input!=this.pista[indice]) {
+                            this.words[indice].estado="rojo";
+                            this.contador++;
+                            this.marcaError(this.contador);
+                            this.compruebafallo(this.contador,this.words);
+                        }
+                }
+            },
+            compruebafallo(contador,array){
+                if(contador==5){
+                    this.mostrar=true;
+                    this.reduceTime(300);
+                    this.resetState();
+                    this.contador=0;
+                    array.forEach(element => {
+                        element.input="";
+                        element.estado="";
 
-        }
+                    });
+                }
+            },
+            closeModal() {
+            this.mostrar = false;
+
+            },
+            compruebaAciertos(aciertos){
+                if(aciertos==5){
+                    this.$router.push("Ranking");
+                }
+            },
+            ...mapActions(useProgressBarStore,["insertaFallo1",
+                                                "insertaFallo2",
+                                                "insertaFallo3",
+                                                "insertaFallo4",
+                                                "insertaFallo5",
+                                                "incrementafallo",
+                                                "marcaError",
+                                                "resetState",]),
+            ...mapActions(useTemporizadorStore,["reduceTime"]),
     },
     computed:{
+
+
         ...mapWritableState(useFinalyWord,["cientifico","clave"]),
+    },
+    components:{
+        unsuccess
     }
 }
 </script>
